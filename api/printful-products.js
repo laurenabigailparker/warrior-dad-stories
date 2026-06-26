43ab56391fbf25742b0ca78ca2163aa853bf9a7f
Lauren Parker <laurenabigailparker@gmail.com>
2026-06-26 14:03:37 -0600

    api fixing

--- api/printful-products.js
+++ api/printful-products.js
@@ -1,12 +1,6 @@
 export default async function handler(req, res) {
   try {
-    if (!process.env.PRINTFUL_API_KEY) {
-      return res.status(500).json({
-        error: "Missing PRINTFUL_API_KEY",
-      });
-    }
-
-    const response = await fetch("https://api.printful.com/store/products", {
+    const response = await fetch("https://api.printful.com/stores", {
       headers: {
         Authorization: `Bearer ${process.env.PRINTFUL_API_KEY}`,
       },
