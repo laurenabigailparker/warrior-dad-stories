0bfe2935fdb15ee415194fe27c749fdc57bc183d
Lauren Parker <laurenabigailparker@gmail.com>
2026-06-26 14:09:35 -0600

    Connect Printful API

--- api/printful-products.js
+++ api/printful-products.js
@@ -1,10 +1,13 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://api.printful.com/stores", {
-      headers: {
-        Authorization: `Bearer ${process.env.PRINTFUL_API_KEY}`,
-      },
-    });
+    const response = await fetch(
+      "https://api.printful.com/store/products",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.PRINTFUL_API_KEY}`,
+        },
+      }
+    );
 
     const data = await response.json();
 
