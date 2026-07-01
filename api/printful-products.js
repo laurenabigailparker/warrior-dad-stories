c460629a5ad8ca03cd3fd6d159989df64fe4eab7
Lauren Parker <laurenabigailparker@gmail.com>
2026-06-30 20:35:45 -0600

    Add Printful product lookup by id

--- api/printful-products.js
+++ api/printful-products.js
@@ -1,6 +1,12 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://api.printful.com/sync/products", {
+    const { id } = req.query;
+
+    const url = id
+      ? `https://api.printful.com/sync/products/${id}`
+      : "https://api.printful.com/sync/products";
+
+    const response = await fetch(url, {
       headers: {
         Authorization: `Bearer ${process.env.PRINTFUL_API_KEY}`,
       },
